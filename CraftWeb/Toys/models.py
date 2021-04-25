d542cdee281fdcdef14f5e9626e3acd458dfb272
Varshini <msvarshini2017@gmail.com>
2021-04-25 19:54:50 +0530

    commit done

---
+++ CraftWeb/Toys/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Customer(models.Model):
+	FirstName=models.CharField(max_length=20,unique=True)
+	LastName=models.CharField(max_length=20)
+	Mobile=models.CharField(max_length=10)
+	Email = models.EmailField(blank=True)
+	Password = models.CharField(max_length=10,null=True)
